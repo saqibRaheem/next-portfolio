63962a68d7947762c38ae1e571a6950695d560f6
saqibRaheem <85112626+saqibRaheem@users.noreply.github.com>
2024-08-06 00:41:50 +0500

    home section complete

---
+++ components/Stats.jsx
@@ -0,0 +1,50 @@
+"use client"
+
+import CountUp from "react-countup"
+
+
+const stats = [
+    {
+        num:14,
+        text: "years of experience"
+    },
+    {
+        num:5,
+        text: "projects complete"
+    },
+    {
+        num:8,
+        text: "Technologies mastered"
+    },
+    {
+        num:500,
+        text: "code commits"
+    },
+   
+]
+function Stats() {
+  return (
+    <section>
+        <div className="container mx-auto">
+            <div className="flex flex-wrap gap-6 max-w-[88vw]
+            mx-auto xl:max-w-none">
+                {stats.map((item, index)=>{
+                    return(
+                        <div className="flex-1 flex gap-4 items-center justify-center xl:justify-start" key={index}>
+                         <CountUp 
+                          end={item.num}
+                          duration={5}
+                          delay={2}
+                          className="text-4xl xl:text-6xl font-extrabold text-accent"
+                         />
+                         <p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80 `}> {item.text} </p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Stats
