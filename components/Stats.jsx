54fc058646fce1adcc1237abd333606720090894
saqibRaheem <85112626+saqibRaheem@users.noreply.github.com>
2024-08-06 16:17:21 +0500

    complete services section

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -24,7 +24,7 @@ const stats = [
 ]
 function Stats() {
   return (
-    <section>
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
         <div className="container mx-auto">
             <div className="flex flex-wrap gap-6 max-w-[88vw]
             mx-auto xl:max-w-none">
